4381031462896981eccb02cc7e3d554131c6f60c
Rafiadnan0666 <fn234561@gmail.con>
2025-07-21 15:03:20 +0700

    meta tag

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -232,6 +232,7 @@ export default function TeamPage() {
 
     container.addEventListener('scroll', handleScroll);
     return () => container.removeEventListener('scroll', handleScroll);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMember, hasMoreMessages]);
 
   const handleSaveSettings = async () => {
@@ -486,6 +487,13 @@ export default function TeamPage() {
   if (team.visibility === 'private' && !isMember && !isOwner) {
     return (
       <Layout>
+        {/* meta tag  */}
+        <title>Join Team t/{team.name}</title>
+        <meta name="description" content={`Join the private team t/${team.name} to view its content.`} />
+        <meta property="og:title" content={`Join Team t/${team.name}`} />
+        <meta property="og:description" content={`Join the private team t/${team.name} to view its content.`} />
+        
+       
         <div className="min-h-screen bg-gradient-to-b from-blue-50 to-purple-50 pt-16">
           <div className="max-w-4xl mx-auto p-4">
             <div className="bg-white rounded-lg shadow-lg p-8 text-center">
